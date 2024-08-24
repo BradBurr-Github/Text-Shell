0288ef254d014a35cd43ff2ed3d0e12b7ab4aa7a
unknown <bradburr@gmail.com>
2024-08-24 06:24:11 -0600

    hard-coded 1 as the id to always get from the IndexedDB

--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -25,8 +25,14 @@ export default class {
     // When the editor is ready, set the value to whatever is stored in indexeddb.
     // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
     getDb().then((data) => {
-      console.info('Loaded data from IndexedDB, injecting into editor');
-      this.editor.setValue(data || localData || header);
+      try {
+        console.info('Loaded data from IndexedDB, injecting into editor');
+        console.log(data)
+        this.editor.setValue(data.content || localData || header);
+        console.log('set DATA to the editor')
+      } catch (err) {
+        console.log(err)
+      }
     });
 
     this.editor.on('change', () => {
