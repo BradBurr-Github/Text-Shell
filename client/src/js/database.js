97dbb9db92ee7d9de10396a2982390c23badd31b
unknown <bradburr@gmail.com>
2024-08-25 00:02:41 -0600

    fixed all the reading and updating functionality to the IndexedDB database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,44 +1,54 @@
 import { openDB } from 'idb';
 
-const initdb = async () =>
-  openDB('jate', 1, {
-    upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
-        return;
-      }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
-    },
-  });
+const initdb = async () => {
+  try {
+    openDB('jate', 1, {
+      upgrade(db) {
+        if (db.objectStoreNames.contains('jate')) {
+          console.log('jate database already exists');
+          return;
+        }
+        db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+        console.log('jate database created');
+      },
+    });
+  } catch (err) {
+      console.log(err)
+  }
+}
 
 // Method that accepts some content and adds it to the database
-export const putDb = async (content) => {
+export const putDb = async (dbIndex,content) => {
   console.log('PUT to the database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ id: 1, content: content });
+  const request = store.put({ id: dbIndex, content: content });
   const result = await request;
   console.log('Data saved to the database', result);
 };
 
-// Method that gets all the content from the database
+// Method to get the record(s) from the IndexedDB database
 export const getDb = async () => {
-  console.log('GET from the database');
-  // Create a connection to the database database and version we want to use.
+  console.log('GET all from the database');
   const jateDb = await openDB('jate', 1);
-  // Create a new transaction and specify the database and data privileges.
   const tx = jateDb.transaction('jate', 'readonly');
-  // Open up the desired object store.
   const store = tx.objectStore('jate');
-  // Use the .getAll() method to get all data in the database.
-  const request = await store.get(1);
-  console.log(`request: ${request}`);
-  // Get confirmation of the request.
+  const request = store.getAll();
   const result = await request;
   console.log('result.value', result);
   return result;
+};
+
+// Method to add a new record to the IndexedDB database, if it doesn't exist
+export const postDb = async (content) => {
+  console.log('Post to the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.add({ content: content });
+  const result = await request;
+  console.log('Data saved to the database', result);
 };
 
 initdb();
--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -1,48 +1,61 @@
 // Import methods to save and get data from the indexedDB database in './database.js'
-import { getDb, putDb } from './database';
+import { getDb, putDb, postDb } from './database';
 import { header } from './header';
 
 export default class {
+
   constructor() {
+    let dbIndex = -1;
     const localData = localStorage.getItem('content');
 
     // check if CodeMirror is loaded
     if (typeof CodeMirror === 'undefined') {
       throw new Error('CodeMirror is not loaded');
-    }
-
-    this.editor = CodeMirror(document.querySelector('#main'), {
-      value: '',
-      mode: 'javascript',
-      theme: 'monokai',
-      lineNumbers: true,
-      lineWrapping: true,
-      autofocus: true,
-      indentUnit: 2,
-      tabSize: 2,
-    });
-
-    // When the editor is ready, set the value to whatever is stored in indexeddb.
-    // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
-    getDb().then((data) => {
-      try {
-        console.info('Loaded data from IndexedDB, injecting into editor');
-        console.log(data)
-        this.editor.setValue(data.content || localData || header);
-        console.log('set DATA to the editor')
-      } catch (err) {
+    }    
+    
+    try {
+      this.editor = CodeMirror(document.querySelector('#main'), {
+        value: '',
+        mode: 'javascript',
+        theme: 'monokai',
+        lineNumbers: true,
+        lineWrapping: true,
+        autofocus: true,
+        indentUnit: 2,
+        tabSize: 2,
+      });
+    } catch (err) {
         console.log(err)
-      }
-    });
+    }
+    
+    // Get the database record in the IndexedDB database and set the "content" to the editor window
+    fetchContent().then((data) => {
+      dbIndex = data[0].id;
+      this.editor.setValue(data[0].content);
+    })
 
     this.editor.on('change', () => {
       localStorage.setItem('content', this.editor.getValue());
     });
 
-    // Save the content of the editor when the editor itself is loses focus
+    // Save the content of the editor when the editor itself loses focus
     this.editor.on('blur', () => {
-      console.log('The editor has lost focus');
-      putDb(localStorage.getItem('content'));
+      putDb (dbIndex, localStorage.getItem('content'));
     });
   }
+}
+
+const fetchContent = async () => {
+
+  // Get the record in IndexDB database
+  let contents = '';
+  let data = await getDb();
+  if (data.length === 0) {
+    // No record was found in IndexDB database - add the header value
+    await postDb(header);
+    // Get the record that was just added
+    data = await getDb();
+  }
+  
+  return data;
 }
